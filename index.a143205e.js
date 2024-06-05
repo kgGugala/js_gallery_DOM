62dc76a427e5f0ec43473caff4bdac8572459bf9
Krzysztof <krzysiek.gugala@gmail.com>
2024-06-05 09:54:04 +0200

    make build

---
+++ index.a143205e.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var n=e.target;n=n.parentElement,t.src=n.href});
+//# sourceMappingURL=index.a143205e.js.map
